36e9ced715301bdbc4600f51e67a5f51b92b5ff7
Martin <mdsugarev@gmail.com>
2022-12-28 15:06:39 +0200

    added header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './Header.module.css'
+import { Grid, Container, TextField, Button } from '@mui/material';
+import Logo from '../logo/Logo';
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+import classNames from "classnames";
+
+
+export default function Header(){
+    return (
+       <Container maxWidth={false} className={classNames(styles['header-container'])}>
+        <Grid container spacing={3} alignItems="center" justifyContent="space-between" >
+            <Grid item xs={'auto'}>
+                <Logo type='default' />
+            </Grid>
+            <Grid item xs={'6'}>
+                <TextField
+                 fullWidth={true}
+                 placeholder="Find items, users and activities"
+                 InputProps={{
+                    className: classNames(styles['header-input']),
+                     startAdornment:(
+                     <InputAdornment position="start">
+                       <SearchIcon className={classNames(styles['header-magnifier-icon'])}/>
+                     </InputAdornment>
+                     )}}
+                />
+            </Grid>
+            <Grid item xs={'auto'}>
+                 <Button variant="text">Home</Button>
+                 <Button variant="text">Activity</Button>
+                 <Button variant="contained">EXPLORE</Button>
+            </Grid>
+        </Grid>
+       </Container> 
+    )
+}
--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import classNames from 'classnames';
 
 export default function Logo({type}){
-return <img className={classNames(styles['logo-container'])} src={ type === 'default' ? "/images/logo.svg" : "/images/logo-muted.svg"} alt="logo-image"/>  
+return <img src={ type === 'default' ? "/images/logo.svg" : "/images/logo-muted.svg"} alt="logo-image"/>  
 }
 
 Logo.defaultProps = {
