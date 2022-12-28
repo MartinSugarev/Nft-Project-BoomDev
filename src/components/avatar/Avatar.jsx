dbcb90dcbeb09f7fb97c689baf4b55ed1dc87361
Martin <mdsugarev@gmail.com>
2022-12-29 00:38:55 +0200

    refactoring

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -5,11 +5,9 @@ import classNames from 'classnames';
 
 export default function Avatar({size = 90, url , verified = false}){
     return (
-        <>
-       <div className={styles.avatar} style={{width: size + 'px', height: size + 'px'}} >
-            <img className={classNames(styles.image)}  src={url} alt="Image"/>
-           { verified ?  <img className={classNames(styles.badge)} src={'/images/verified.svg'}/> : <img className={classNames(styles.badge)} style={{display: "none"}} src={'/images/verified.svg'} /> }
+       <div className={classNames(styles['avatar'])} style={{width: size + 'px', height: size + 'px'}} >
+            <img className={classNames(styles['image'])}  src={url} alt="Image"/>
+            <img className={classNames(styles['badge'])} style={{display: verified ? "block" : 'none' }} src={'/images/verified.svg'}/> 
        </div>
-       </>
     );
     };
