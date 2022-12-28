15ec61b6a9652a8618836e8c57039dfd17571f55
Martin <mdsugarev@gmail.com>
2022-12-28 15:53:51 +0200

    ref2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Header.module.css'
+import styles from './Header.module.scss'
 import { Grid, Container, TextField, Button } from '@mui/material';
 import Logo from '../logo/Logo';
 import SearchIcon from '@mui/icons-material/Search';
