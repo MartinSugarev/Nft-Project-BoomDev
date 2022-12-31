3236e0894281e2873951c158659b207aa6cb3b97
Martin <mdsugarev@gmail.com>
2022-12-31 23:47:36 +0200

    againg

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,15 +13,15 @@ export default function Footer() {
     return (
         <Container maxWidth={false} className={classNames(styles['footer-container'])}>
             <Grid container justifyContent="space-around" alignItems="center">
-               <Grid item xs={"auto"}>
+               <Grid item xs="auto">
                    <Logo type="muted" />
                </Grid>
-               <Grid item xs={"auto"}>
+               <Grid item xs="auto">
                    <Typography className={classNames(styles['footer-paragraph'])} variant="subtitle2">
                    Bum All Rights Reserved 2021
                    </Typography>
                </Grid>
-               <Grid item xs={"auto"}>
+               <Grid item xs="auto">
                   <Button sx={{color: theme.palette.text.secondary, padding: 1}} size="small" variant="text">Privacy Policy</Button>
                   <Button sx={{color: theme.palette.text.secondary, padding: 1}}  size="small" variant="text">Cookie Policy</Button>
                </Grid>
