1162ac9473371b2ab61d1e28cb2d81dba709c61d
Martin <mdsugarev@gmail.com>
2023-01-02 00:49:19 +0200

    e

--- src/components/how/Step.jsx
+++ src/components/how/Step.jsx
@@ -2,11 +2,13 @@ import styles from './Step.module.scss';
 import { Grid, Typography} from '@mui/material';
 import classNames from "classnames";
 import React from 'react';
+import { useTheme } from '@mui/material/styles';
 
 export default function Step({number, title, description}) {
+    const theme = useTheme()
     return (
         <Grid container className={classNames(styles['step-layout'])} m={0}>
-            <Grid item xs={3} className={classNames(styles['step-number'])}>
+            <Grid item xs={3} className={classNames(styles['step-number'])} sx={{background: theme.palette.primary.main}}>
                   {number}
             </Grid>
             <Grid item xs={9} className={classNames(styles['step-text'])}>
