883ba549dd83e760315e3d20f2d028aa9744d60e
Martin <mdsugarev@gmail.com>
2023-01-05 00:28:32 +0200

    added variant

--- src/components/product/ProductInfoLikes.jsx
+++ src/components/product/ProductInfoLikes.jsx
@@ -10,7 +10,7 @@ export default function ProductInfoLikes({amount = 0}) {
     const theme = useTheme()
     return (
         <div className={classNames(styles['product-info-likes'])}>
-            <Chip icon={<FavoriteIcon fontSize="small" style={{color: 'rgba(36,242,94)'}} />} sx={{background: 'rgba(36,242,94, 0.2)', color: theme.palette.secondary.main, fontWeight: '600', border: '2px solid rgba(36,242,94)'}}  label={millify(amount)} className={classNames(styles['likes'])}/>
+            <Chip icon={<FavoriteIcon fontSize="small" style={{color: 'rgba(36,242,94)'}} />} variant="outlined" sx={{background: 'rgba(36,242,94, 0.2)', color: theme.palette.secondary.main, fontWeight: '600', border: '2px solid rgba(36,242,94)'}}  label={millify(amount)} className={classNames(styles['likes'])}/>
         </div>
     )
 }
--- src/components/product/ProductInfoStatus.jsx
+++ src/components/product/ProductInfoStatus.jsx
@@ -9,7 +9,7 @@ export default function ProductInfoStatus() {
     const theme = useTheme()
     return (
         <div className={classNames(styles['product-info-status'])}>
-            < Chip icon={<CircleIcon fontSize="small" />} variant='outlined' label="LIVE" sx={{background: theme.palette.secondary.main, color: theme.palette.background.default, fontWeight: '600'}} className={classNames(styles['status'])}/>
+            < Chip icon={<CircleIcon fontSize="small" />}  label="LIVE" sx={{background: theme.palette.secondary.main, color: theme.palette.background.default, fontWeight: '600'}} className={classNames(styles['status'])}/>
         </div>
     )
 }
