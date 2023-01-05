957cf6a1df5a09f7a28de109a4bc0100d2df6426
Martin <mdsugarev@gmail.com>
2023-01-05 13:41:01 +0200

    added changes

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -7,19 +7,19 @@ import Countdown from '../countdown/Countdown';
 export default function ProductInfoTimer({timeEnd, onTimeEnd}) {
     return (
         <div className={classNames(styles['product-info-timer'])}>
-            <Grid container >
+            { timeEnd && <Grid container >
                <Grid item xs={12} className={classNames(styles['title-container'])}>
                    <Typography variant="h4" className={classNames(styles['title'])}>
                        ends in
                    </Typography>
                </Grid>
-               {timeEnd && <Grid item xs={12} className={classNames({
+                <Grid item xs={12} className={classNames({
                    [styles['timer']]: true,
                    [styles['active']]:  timeEnd ? true : false
                 })}>
                     <Countdown timeLeft={timeEnd} onTimeEnd={onTimeEnd}/>
-               </Grid>}
-            </Grid>
+               </Grid>
+            </Grid>}
             </div>
     )
 }
