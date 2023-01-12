5e5c8b73d30f9751114201c8f3c780f1656578d1
Martin <mdsugarev@gmail.com>
2023-01-12 14:22:37 +0200

    refactoring

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -14,7 +14,7 @@ export default function ActivityListItem({user, created_at, nft, type = "like"})
 
     return (
         <Container maxWidth className={classNames(styles["activity-list-item"])} sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
-                 <Avatar verified={user?.verified} url={user?.avatar?.url} />
+                <Avatar verified={user?.verified} url={user?.avatar?.url}></Avatar>
                 <Box component="div">
                 <Typography sx={{lineHeight: '1'}}>
                    <b>{user?.name}</b> {type === 'like' ? 'liked' : 'bought'} <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}}  href="/">{nft?.name}</Link> by <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}} href="/">{nft?.owner?.username}</Link>
