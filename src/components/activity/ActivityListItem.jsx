ee7b25c9663d33d88100c8ce33cbe67226d231ab
Martin <mdsugarev@gmail.com>
2023-01-12 18:05:55 +0200

    addd path

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -1,8 +1,8 @@
 import styles from "./ActivityListItem.module.scss";
 import classNames from "classnames";
-import {Typography, Container, Box} from '@mui/material';
+import {Typography, Box} from '@mui/material';
 import React from 'react';
-import Avatar from '../avatar/Avatar';
+import Avatar from '../../components/avatar/Avatar';
 import Link from '../link/Link';
 import { useTheme } from '@mui/material/styles';
 import { formatDistance, parseISO } from "date-fns";
@@ -13,16 +13,16 @@ export default function ActivityListItem({user, created_at, nft, type = "like"})
     const theme = useTheme()
 
     return (
-        <div  className={classNames(styles["activity-list-item"])}>
-                <Avatar verified={nft.owner.verified} url={user.avatar?.url}/>
-                <Box component="div">
+        <div className={classNames(styles["activity-list-item"])}>
+                <Avatar verified={user.verified} url={user.avatar?.url}/>
+            <Box component="div">
                 <Typography sx={{lineHeight: '1'}}>
                    <b>{user?.name}</b> {type === 'like' ? 'liked' : 'bought'} <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}}  href="/">{nft?.name}</Link> by <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}} href="/">{nft?.owner?.username}</Link>
                 </Typography>
                 <Typography className={classNames(styles["activity-secondParagraph"])}>
                 {created_at && formatDistance(parseISO(created_at),new Date(), { addSuffix: true })}
                 </Typography>
-                </Box> 
+            </Box> 
         </div>
     )
 }
