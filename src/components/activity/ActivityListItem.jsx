a51cc24bc238a288a1017843a2baa7b7d046a1c9
Martin <mdsugarev@gmail.com>
2023-01-12 19:18:38 +0200

    rwew

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -15,14 +15,14 @@ export default function ActivityListItem({user, created_at, nft, type = "like"})
     return (
         <div className={classNames(styles["activity-list-item"])}>
                 <Avatar url={user.avatar.url} verified={user.verified}/>
-            <Box component="div">
+            
                 <Typography sx={{lineHeight: '1'}}>
                    <b>{user.name}</b> {type === 'like' ? 'liked' : 'bought'} <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}}  href="/">{nft.name}</Link> by <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}} href="/">{nft.owner.username}</Link>
                 </Typography>
                 <Typography className={classNames(styles["activity-secondParagraph"])}>
                 {created_at && formatDistance(parseISO(created_at),new Date(), { addSuffix: true })}
                 </Typography>
-            </Box> 
+            
         </div>
     )
 }
