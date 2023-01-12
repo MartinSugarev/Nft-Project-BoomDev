49d807ed4210c5fe77b2894ff87e7cd251281bf9
Martin <mdsugarev@gmail.com>
2023-01-12 20:13:03 +0200

    oo

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -20,9 +20,9 @@ export default function ActivityListItem({user, created_at, nft, type = "like"})
                 <Typography sx={{lineHeight: '1'}}>
                    <b>{user.name}</b> {type === 'like' ? 'liked' : 'bought'} <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}}  href="/">{nft.name}</Link> by <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}} href="/">{nft.owner.username}</Link>
                 </Typography>
-                <Typography className={classNames(styles["activity-secondParagraph"])}>
+                {/* <Typography className={classNames(styles["activity-secondParagraph"])}>
                 {created_at && formatDistance(parseISO(created_at),new Date(), { addSuffix: true })}
-                </Typography>
+                </Typography> */}
             </Box> 
         </div>
     )
