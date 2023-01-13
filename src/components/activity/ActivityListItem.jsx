dcc95981960cc69307a14c253465b11c79349a8d
Martin <mdsugarev@gmail.com>
2023-01-13 11:55:10 +0200

    gfdsa

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -14,8 +14,8 @@ export default function ActivityListItem({user, created_at, nft, type = "like"})
     const theme = useTheme()
 
     return (
-        <div className={classNames(styles["activity-list-item"])}>
-                <Card user={user}/>
+        < >
+                <Avatar url={user.avatar.url} verified={user.verified}/>
             <Box component="div">
                 <Typography sx={{lineHeight: '1'}}>
                  <b>{user.name}</b> {type === 'like' ? 'liked' : 'bought'} <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}}  href="/">{nft.name}</Link> by <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}} href="/">{nft.owner.username}</Link>
@@ -24,7 +24,7 @@ export default function ActivityListItem({user, created_at, nft, type = "like"})
                 {created_at && formatDistance(parseISO(created_at),new Date(), { addSuffix: true })}
                 </Typography>
             </Box> 
-        </div>
+        </>
     )
 }
 
