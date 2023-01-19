6912c8f105183aaa5bd1d633800fb1e46862e1d9
Martin <mdsugarev@gmail.com>
2023-01-20 00:29:29 +0200

    added size prop

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -6,7 +6,7 @@ import Avatar from '../avatar/Avatar';
 import Link from '../link/Link';
 import { useTheme } from '@mui/material/styles';
 import { formatDistance, parseISO } from "date-fns";
-import Card from '../card/Card'
+
 
 
 export default function ActivityListItem({user, created_at, nft, type = "like"}) {
@@ -15,7 +15,7 @@ export default function ActivityListItem({user, created_at, nft, type = "like"})
 
     return (
         <div className={classNames(styles["activity-list-item"])}>
-                <Avatar url={user.avatar.url} verified={user.verified}/>
+                <Avatar url={user.avatar.url} verified={user.verified} size/>
             <Box component="div">
                 <Typography sx={{lineHeight: '1'}}>
                  <b>{user.name}</b> {type === 'like' ? 'liked' : 'bought'} <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}}  href="/">{nft.name}</Link> by <Link style={{color: theme.palette.secondary.main, textDecoration: 'underlined'}} href="/">{nft.owner.username}</Link>
