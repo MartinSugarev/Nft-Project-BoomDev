a65d690b94ba86d555ab17b3a60e0c16ac9e6596
Martin <mdsugarev@gmail.com>
2023-01-25 23:58:53 +0200

    changed url

--- pages/product/[id]/index.jsx
+++ pages/product/[id]/index.jsx
@@ -8,16 +8,16 @@ import { useRouter } from "next/router";
 
 export default function Index() {
   const router = useRouter();
-  const { id } = router.query
+  const { id } = router.query;
+  const baseUrl = process.env.apiUrl
 
   const [product, setProduct] = useState();
 
   useEffect(async () => {
 
     try {
-      const result = await fetch(`https://project-4-api.boom.dev/nfts/${id}`)
+      const result = await fetch(`${baseUrl}/nfts/${id}`)
       const response =  await result.json()
-      console.log(response);
       
       setProduct(response)
     } catch (error) {
